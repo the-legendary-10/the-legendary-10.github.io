7447ebe911024472e46e785ce1edde2441dc4051
Backbone-js-Demo <mistu@wir004.de>
2014-05-28 14:04:53 +0200

    Scripte ausgelagert, zweites Textfeld hinzugefügt

--- js/mampf.js
+++ js/mampf.js
@@ -1,1 +1,9 @@
-/*Main JavaScript file for the Mampf! Application */
+$( document ).on( "pageinit", "#main-page", function() {
+    $( document ).on( "swiperight", "#main-page", function( e ) {
+        if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
+            if ( e.type === "swiperight"  ) {
+                $( "#left-panel" ).panel( "open" );
+            }
+        }
+    });
+});

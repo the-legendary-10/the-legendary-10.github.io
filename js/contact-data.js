897151623ef20c3716a6847f828d3d66894e4d37
Daniel <info@dwstoll.de>
2014-05-31 13:53:27 +0200

    First draft of contact/group functionality

---
+++ js/contact-data.js
@@ -0,0 +1,16 @@
+// // Test data for contacts ... Array of objects
+
+var contacts = [
+         {firstName: "Simon", lastName: "Rech", phoneNumber: "4912345678",},
+         {firstName: "Lisa", lastName: "Hanstein", phoneNumber: "49123654678",}, 
+         {firstName: "Patrick", lastName: "Heinze", phoneNumber: "4975454458",},    
+         {firstName: "Christian", lastName: "Müller", phoneNumber: "4933335678",}, 
+         {firstName: "Claudia", lastName: "Meier", phoneNumber: "49155555678",}, 
+         {firstName: "Daniel", lastName: "Stoll", phoneNumber: "49133333555678",}, 
+         {firstName: "Marc-Oliver", lastName: "Seidler", phoneNumber: "49166665678",}, 
+         {firstName: "Christoph", lastName: "Bey", phoneNumber: "4912777678",}, 
+         {firstName: "Nicolas", lastName: "Koch", phoneNumber: "49123888878",},
+         {firstName: "Philipp", lastName: "Schramm", phoneNumber: "49123999678",},
+         {firstName: "Ulrike", lastName: "Piontek", phoneNumber: "494101010678",},
+         {firstName: "Kevin", lastName: "Sandermann", phoneNumber: "49313131678",} 
+         ];
---
+++ js/group-edit-page.js
@@ -0,0 +1,143 @@
+
+// Function for sorting Contacts by first letter of first name
+function compareLettersContacts(a,b) {
+  if (a.firstName > b.firstName)
+     return 1;
+  if (a.firstName < b.firstName)
+    return -1;
+  return 0;
+}
+
+
+//------------------------------------------------------------------------------
+
+// Function checks whether md5-cash value is already in the local storage.
+// It is used for the creation of the group-edit-page. Based on the result of this function, all checkboxes of items, which are already in the storage, will be checked. 
+function IsContactInStorage(md5number) {
+
+		if(localStorage.getItem('group')== 0 || localStorage.getItem('group') == undefined ){ 
+		
+				return false;
+				
+		} else {
+		
+				phoneNumbers = localStorage.getItem('group');
+				phoneNumbersArray = phoneNumbers.split(",");
+						
+				for (var k = 0; k < phoneNumbersArray.length; k++) {
+					if(phoneNumbersArray[k] == md5number) {
+						return true;
+					}
+				}
+				
+				return false;
+		
+		}
+	
+}
+
+
+
+//------------------------------------------------------------------------------
+
+// Function for creating a dynamic listview, displaying all contacts in the adressbook
+function dynamicListView(contacts) {
+
+        	    // A list item is created for each contact of the adressbook
+        	    	$.each( contacts, function( i, contact ) {
+     
+        	        // Phone number is converted to md5-hash value
+        	    	hashNumber = $.md5(contact.phoneNumber);
+        	    	
+
+        	                	        
+        	        // Is contact already in the storage? If yes, checkbox will be checked 
+        	           hashString = '#' + hashNumber;
+        	                   	       
+        	           var checkedString = "";
+        	        
+        	           if(IsContactInStorage(hashNumber)) {
+        	           		checkedString = "checked";
+        	           
+        	           }
+        	           
+        	           //  Appending new list view element to the listview
+        	           $('#groupList').append('<li style=" padding: 0em;  "><label class="listview" ><a  href="#" class="listview" ><div class="listview" >' + contact.firstName + ' ' + contact.lastName + '</div><input data-iconpos="left"  class="allCheckboxesContacts" name="' + hashNumber + '" id="' + hashNumber + '" type="checkbox" value="true" ' + checkedString + ' /></a></label></li>');
+        	        
+     
+        	       });  
+        	       
+        	       
+        	        	       
+        	       // Updating listview
+        	        $('#groupList').listview('refresh');
+        	        $("[type=checkbox]").checkboxradio();
+        	        
+        	        $("[data-role=controlgroup]").controlgroup("refresh");
+        	   
+
+}
+
+
+//------------------------------------------------------------------------------
+// // Probably we do not need it. 
+// Enables the save-button, if at least one checkbox is checked 
+//$(document).on("change", ".allCheckboxesContacts", function () {
+//  var checkkboxesChecked = $('.allCheckboxesContacts:checked')
+//    if (checkkboxesChecked.length > 0) {
+//        $("#saveContacts").removeClass('ui-disabled');
+//
+//    } else {
+//        $("#saveContacts").addClass('ui-disabled');
+//       
+//    }
+//});
+
+
+//------------------------------------------------------------------------------
+// Functionality for save button. MD5-hash values of all selected contacts (phone numbers) will be saved in the localStorage (key in storage: "group")
+$(function() {
+      $("#saveContacts").click( function() {
+           
+           
+           arrayPhoneNumbers = new Array();
+           
+           
+         // Get all unchecked line items with their ID (= phoneNumber)... will be stored in the Array arrayPhoneNumbers
+         $('input[type="checkbox"]').filter('.allCheckboxesContacts').each(function(){
+           	   if($(this).is(':checked')){
+           	   
+           	   arrayPhoneNumbers.push(this.getAttribute('id'));
+           	    
+           	   }
+           	
+           	});
+           	
+           	
+           	// Store MD5-hash phonenumbers of selected contacts in the local storage
+           localStorage.setItem("group", arrayPhoneNumbers);
+           
+           
+           // Trigger update of group-overview-page
+            $( "#group-over-list" ).children().remove();
+            $( "#group-overview-page" ).trigger("pagecreate");
+           	 
+           	 
+           }
+      );
+});   
+//------------------------------------------------------------------------------
+
+// Is called, while 'contact-page' is being created (only once)
+$(document).on("pagecreate","#group-edit-page",function(){
+		      
+		 
+		    // Sorting of contacts (specified in contact-data.js) by first letter of first name
+		    contacts.sort(compareLettersContacts);
+		    
+
+		    // Creation of dynamic listview
+		    dynamicListView(contacts);
+		    
+		     		
+});

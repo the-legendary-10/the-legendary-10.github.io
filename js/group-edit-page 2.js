ea095e30ea4ad94f0312a1268eecdc3429e45777
Daniel <info@dwstoll.de>
2014-06-01 10:10:59 +0200

    Bugfixing checkboxes

---
+++ js/group-edit-page 2.js
@@ -0,0 +1,139 @@
+
+// Function for sorting Contacts by first letter of first name
+function compareLettersContacts(a,b) {
+  if (a.firstName > b.firstName)
+     return 1;
+  if (a.firstName < b.firstName)
+    return -1;
+  return 0;
+}
+
+
+//------------------------------------------------------------------------------
+
+// Function checks whether md5-cash value is already in the local storage.
+// It is used for the creation of the group-edit-page. Based on the result of this function, all checkboxes of items, which are already in the storage, will be checked. 
+function IsContactInStorage(md5number) {
+
+		if(localStorage.getItem('group')== 0 || localStorage.getItem('group') == undefined ){ 
+		
+				return false;
+				
+		} else {
+		
+				phoneNumbers = localStorage.getItem('group');
+				phoneNumbersArray = phoneNumbers.split(",");
+						
+				for (var k = 0; k < phoneNumbersArray.length; k++) {
+					if(phoneNumbersArray[k] == md5number) {
+						return true;
+					}
+				}
+				
+				return false;
+		
+		}
+	
+}
+
+
+
+//------------------------------------------------------------------------------
+
+// Function for creating a dynamic listview, displaying all contacts in the adressbook
+function dynamicListView(contacts) {
+
+        	    // A list item is created for each contact of the adressbook
+        	    	$.each( contacts, function( i, contact ) {
+     
+        	        // Phone number is converted to md5-hash value
+        	    	hashNumber = $.md5(contact.phoneNumber);
+        	    	
+
+        	                	        
+        	        // Is contact already in the storage? If yes, checkbox will be checked        	       
+        	           var checkedString = "";
+        	        
+        	           if(IsContactInStorage(hashNumber)) {
+        	           		checkedString = "checked";
+        	           
+        	           }
+        	           
+        	           //  Appending new list view element to the listview
+        	           // id gets the letter 'E' (for Edit-page) as prefix, since there are already listitems with IDs based on hash numbers (on group-overview-page).
+        	           // However the value of name is the hash number without a prefix.
+        	           $('#groupList').append('<li style=" padding: 0em;  "><label class="listview" ><a  href="#" class="listview" ><div class="listview" >' + contact.firstName + ' ' + contact.lastName + '</div><input data-iconpos="left"  class="allCheckboxesContacts" name="' + hashNumber + '" id="E' + hashNumber + '" type="checkbox" value="true" ' + checkedString + ' /></a></label></li>');
+        	        
+     
+        	       });  
+        	               	        	       
+        	       // Updating listview
+        	        $('#groupList').listview('refresh');
+        	        $("[type=checkbox]").checkboxradio();
+        	        $("[data-role=controlgroup]").controlgroup("refresh");
+        	   
+
+}
+
+
+//------------------------------------------------------------------------------
+// // Probably we do not need it. 
+// Enables the save-button, if at least one checkbox is checked 
+//$(document).on("change", ".allCheckboxesContacts", function () {
+//  var checkkboxesChecked = $('.allCheckboxesContacts:checked')
+//    if (checkkboxesChecked.length > 0) {
+//        $("#saveContacts").removeClass('ui-disabled');
+//
+//    } else {
+//        $("#saveContacts").addClass('ui-disabled');
+//       
+//    }
+//});
+
+
+//------------------------------------------------------------------------------
+// Functionality for save button. MD5-hash values of all selected contacts (phone numbers) will be saved in the localStorage (key in storage: "group")
+$(function() {
+      $("#saveContacts").click( function() {
+           
+           
+           arrayPhoneNumbers = new Array();
+           
+           
+         // Get all unchecked line items with their ID (= phoneNumber)... will be stored in the Array arrayPhoneNumbers
+         $('input[type="checkbox"]').filter('.allCheckboxesContacts').each(function(){
+           	   if($(this).is(':checked')){
+           	   
+           	   arrayPhoneNumbers.push(this.getAttribute('name'));
+           	    
+           	   }
+           	
+           	});
+           	
+           	
+           	// Store MD5-hash phonenumbers of selected contacts in the local storage
+           localStorage.setItem("group", arrayPhoneNumbers);
+           
+           
+           // Trigger update of group-overview-page
+            $( "#group-overview-page" ).trigger("pagecreate");
+           	 
+           	 
+           }
+      );
+});   
+//------------------------------------------------------------------------------
+
+// Is called, while 'contact-page' is being created 
+$(document).on("pagecreate","#group-edit-page",function(){
+		      
+		 
+		    // Sorting of contacts (specified in contact-data.js) by first letter of first name
+		    contacts.sort(compareLettersContacts);
+		    
+
+		    // Creation of dynamic listview
+		    dynamicListView(contacts);
+		    
+		     		
+});
---
+++ js/group-overview-page 2.js
@@ -0,0 +1,169 @@
+// Function results an object (= contact) with firstName, lastName and phoneNumber based on md5 phoneNumber
+function searchPhoneNumber(md5number) {
+var contactObj;
+  $.each( contacts, function( i, contact ) {
+  
+  		if(md5number == $.md5(contact.phoneNumber)) {
+  		
+  		 contactObj = {"phoneNumber": contact.phoneNumber,
+  		 		       "firstName": contact.firstName,
+  		               "lastName": contact.lastName
+  		 };
+  		    	
+  		}
+  
+  });
+  
+  return contactObj;
+       		
+}
+
+
+
+
+
+
+//------------------------------------------------------------------------------
+// Function defines text that should appear in the popup after clicking on a delete icon
+
+$(function() {
+
+         $("body").on("click", ".deleteField",function(e) {
+         
+            // get MD5-Hash-Number
+            var number= $(this).parent().attr("id");
+            
+            
+            // get contact based on MD5-Hash-NUmber
+            var contactObj = searchPhoneNumber(number);
+            
+            
+            //append text to popup
+            var text = '<p class="toDelete" id ="' + number +'">Möchtest du den Kontakt <b>' +  contactObj.firstName + ' ' + contactObj.lastName + '</b> <br />aus der Gruppe entfernen?</p>';
+            
+             $("#popup-content").append(text);
+            
+            
+            
+         });    
+         
+         
+}); 
+
+//------------------------------------------------------------------------------
+// Removes individual text in the popup after closing the popup
+$(function() {
+
+         $( "#popup-action" ).bind({
+              popupafterclose: function(event, ui) {
+                  
+                     
+               $(".toDelete").remove();
+                     
+                }
+          });
+
+}); 
+
+//------------------------------------------------------------------------------
+// Defines action after clicking on button "Löschen" ("deleteCon") in the popup
+
+$(function() {
+      $("#deleteCon").click( function() {
+      
+      	  // get MD5-Hash-Number of selected contact
+          var hashNumber = $(".toDelete").attr("id");    
+         
+        
+
+           // Get saved group from local storage
+           var groupLocalStorage = localStorage.getItem('group');
+           
+                     
+           // Split the local storage value in parts (one part = MD5-Hash-Number)
+           var groupLocalStorageArray = groupLocalStorage.split(",");
+           
+           
+           // Identify the item to be deleted
+           var itemToBeDeleted;
+           for(var i = 0; i< groupLocalStorageArray.length;i++) {
+           			if(groupLocalStorageArray[i] == hashNumber) {
+           			    itemToBeDeleted = i; 
+           			};          	           
+           };
+
+          
+          
+          // Remove the item from the array
+          groupLocalStorageArray.splice(itemToBeDeleted,1);
+ 
+
+		  // Save the new Array in the local storage
+          localStorage.setItem('group', groupLocalStorageArray);
+            
+            
+            
+          // Remove the item from the group-overview-page  
+          var hashNumberString = '#' + hashNumber;
+          $('li').remove(hashNumberString);
+          
+          
+          // Uncheck the checkbox in the group-edit-page (id: E (prefix) + hashNumber)
+          $("#E" + hashNumber).prop("checked", false).checkboxradio("refresh");
+             
+            
+           
+          // Close the popup
+           $( "#popup-action" ).popup("close");
+                 	
+     	}
+	);
+}); 
+
+
+
+
+//------------------------------------------------------------------------------
+
+// Is called, while 'contact-overview-page' is being created and after changes in the group-edit-page (save-button)
+$(document).on("pagecreate","#group-overview-page",function(){
+    
+    
+    // Initialization of group-over-list ... Remove all children
+     $( "#group-over-list" ).children().remove();
+    
+   
+    // Creation of user interface (information message if no contact or listview with contacts)
+    
+   
+   	   // If no contact is stored in the local Storage, show an information message
+   		if(localStorage.getItem('group')== 0 || localStorage.getItem('group') == undefined ){ 
+   		  $('#group-over-list').append('<div class="infobox" ><b>Hinweis:</b> <br/> Aktuell befinden sich <b>keine Kontakte</b> in Deiner Mitagessen-Gruppe. Füge welche über "Gruppe bearbeiten" hinzu.</div'); 
+   		  
+   		
+   		} else{ 					
+   				// Get group-value with md5-hash-numbers and convert it to an array	
+   				phoneNumbers = localStorage.getItem('group');
+   				phoneNumbersArray = phoneNumbers.split(",");
+   				
+   				
+   				// Generate listitem for each md5-hash-number (contact) and append it to the listview
+   			    for (var k = 0; k < phoneNumbersArray.length; k++) {
+   			    
+   				  contactObj = searchPhoneNumber(phoneNumbersArray[k]);
+   			
+   			      appendString  =	'<li id="' + $.md5(contactObj.phoneNumber) + '"><a class="read-only-list">' + contactObj.firstName + ' ' + contactObj.lastName + '</a><a href="#popup-action" class="deleteField" data-rel="popup"  data-rel="popup" data-position-to="window" data-transition="fade" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-icon="delete" data-iconpos="right" >Delete</a></li>';
+   				
+   				   $('#group-over-list').append(appendString);
+   				
+   				}
+   				
+   			
+   			   // Update listview
+               $('#group-over-list').listview('refresh');
+   				
+   								   			
+   		}  
+   		       
+      
+});

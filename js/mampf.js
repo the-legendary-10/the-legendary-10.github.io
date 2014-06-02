fbeed00e372342e634e5ab8d6eab5b87ac056592
Backbone-js-Demo <Backbone-js-Demo@users.noreply.github.com>
2014-06-02 10:15:20 +0200

    Time komplett auf JSON umgestellt

--- js/mampf.js
+++ js/mampf.js
@@ -13,25 +13,20 @@ function SaveMyTime(){
 	myDate.Date = $("#mydate").val();;
 	myDate.FromTime = $("#fromtime").val();
 	myDate.ToTime = $("#totime").val();
-	var Date = JSON.stringify(myDate);
+	var date = JSON.stringify(myDate);
 	
-	localStorage.Date=Date;
+	localStorage.date=date;
 	location.reload(true);
 }
 
 $("#time-page").ready(function(){
-	
-	var date = localStorage.Date.date;
-	var fromtime = localStorage.Date.FromTime;
-	var totime= localStorage.Date.ToTime;
-	
+	var date=localStorage.Date;
+
 	if(date != null){
-		if(fromtime != null){
-			if(totime != null){
-				var d = new Date(date);
-				$( "#LastValues" ).append("<p>Die aktuell gespeicherten Werte sind: am "+d.getDate()+"."+d.getMonth()+"."+d.getUTCFullYear()+" von: "+fromtime+" bis "+totime+"</p>");
-				$( "#ButtonSaveTime").html("Ändern");
-			}
-		}
+		var myDate=JSON.parse(date);
+		var d = new Date(myDate.Date);
+		$( "#LastValues" ).append("<p>Die aktuell gespeicherten Werte sind: am "+d.getDate()+"."+d.getMonth()+"."+d.getUTCFullYear()+" von: "+myDate.FromTime+" bis "+myDate.ToTime+"</p>");
+		$( "#ButtonSaveTime").html("Ändern");
 	}	
+	
 });

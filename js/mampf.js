6c8a35c4cac6258874f198f70c022d051a4a8218
Backbone-js-Demo <Backbone-js-Demo@users.noreply.github.com>
2014-06-02 11:07:57 +0200

    Umstellung auf Array

--- js/mampf.js
+++ js/mampf.js
@@ -6,25 +6,46 @@ $(function () {
   $("[data-role=panel]").panel().enhanceWithin();
 });
 
+var dates = new Array();
 function SaveMyTime(){	
 	var myDate = new Object();
 	myDate.Date = $("#mydate").val();;
 	myDate.FromTime = $("#fromtime").val();
 	myDate.ToTime = $("#totime").val();
-	var date = JSON.stringify(myDate);
+	var JSONdate = JSON.stringify(myDate);
 	
-	localStorage.date=date;
+	var old = localStorage.getItem("dates");
+	if(old!=null){
+		old = old+"#"+JSONdate;
+	}
+	else{
+		old= JSONdate;
+	}
+	localStorage.dates=old;
 	location.reload(true);
 }
 
 $("#time-page").ready(function(){
-	var date=localStorage.Date;
+	var dates=localStorage.dates;
 
-	if(date != null){
-		var myDate=JSON.parse(date);
+	if(dates != null){
+		console.log(dates);
+		var myDates = dates.split('#');
+		console.log(myDates);
+		console.log(myDates[0]);
+		
+		for (var date in myDates){
+			console.log(date);
+		}
+		/*
+		var myDate=JSON.parse(date[0]);
 		var d = new Date(myDate.Date);
 		$( "#LastValues" ).append("<p>Die aktuell gespeicherten Werte sind: am "+d.getDate()+"."+d.getMonth()+"."+d.getUTCFullYear()+" von: "+myDate.FromTime+" bis "+myDate.ToTime+"</p>");
 		$( "#ButtonSaveTime").html("Ändern");
-	}	
+		*/
+	}
+	else{
+		//alert("null");
+	}
 	
 });

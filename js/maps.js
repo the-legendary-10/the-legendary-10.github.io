32c00542953660be644523ad972a1518368871d4
Patrick H <dalamardunkelelf@googlemail.com>
2014-06-04 13:43:36 +0200

    Maps infobox and checkbox
    ...and comments added

--- js/maps.js
+++ js/maps.js
@@ -1,15 +1,24 @@
 var userLocation = undefined;
 $(document).on("pagecreate","#main-page",function(){
     var map = null; 
-	var userLocation = null;
+	var userLocation = null; //Location which will be used for lunch-date calculation
+	var deviceLocation = null; //Device location
+	var chosenLocation = null; //Location chosen by user
+	
 	//set pin for own location
 	var myLocPin = null;
+	
+	//initialize map
     GetMap();
 	var mapHeight = document.documentElement.clientHeight * 0.9;
 	$( "#mapDiv" ).height(mapHeight);
 
 	var mapWidth = document.documentElement.clientWidth * 0.7;
 	$( "#mapDiv" ).width(mapWidth);
+	
+	//disable radio buttons (will be enabled, if specific location is available
+	$("#locationRadios1").attr("disabled", true);
+	$("#locationRadios2").attr("disabled", true);
 	
 	$("#telephoneButton").on("click",savePhoneNumber);
 	var ownPhone = localStorage.getItem("ownphone");
@@ -22,34 +31,47 @@ function GetMap() {
             Obtain a key by signing up for a developer account at 
             http://www.microsoft.com/maps/developers/ */ 
         var cred = "Ak4YyNZDcqdffzA8v6abqlO_tlEhDs4ZK30zbET6AP9tTXokOsVX0E4FtaJEyAUB"; 
+		
         // Initialize map 
         map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), 
 				{ credentials: cred });
-		Microsoft.Maps.Events.addHandler(map, 'click',getLatlng );			
+		Microsoft.Maps.Events.addHandler(map, 'click',getLatlng );	
+		
         // Check if browser supports geolocation 
         if (navigator.geolocation) { 
             navigator.geolocation.getCurrentPosition(locateSuccess, locateFail);
         } 
         else { 
             alert('I\'m sorry, but Geolocation is not supported in your current browser.'); 
+			$("#locationRadios1").attr("disabled", true);
         } 
     } 
 
     // Successful geolocation 
     function locateSuccess(loc) { 
         // Set the user's location 
-        userLocation = new Microsoft.Maps.Location(loc.coords.latitude, loc.coords.longitude); 
+        deviceLocation = new Microsoft.Maps.Location(loc.coords.latitude, loc.coords.longitude); 
+		userLocation = deviceLocation;
+		
         // Zoom in on user's location on map 
         map.setView({ center: userLocation, zoom: 13 });
             // Add a pin to the center of the map
-            var pin = new Microsoft.Maps.Pushpin(userLocation, { draggable: false});
+            var pin = new Microsoft.Maps.Pushpin(userLocation, {text: '1'}, { draggable: false});
             map.entities.push(pin);
-        // Draw circle of area where user is located 
+			
+		//enable "Gerätestandort" radio button and check it	
+		$("#locationRadios1").attr("disabled", false);	
+		$("#locLabel1").removeClass("read-only-list");
+		$("#locationRadios1").attr("checked",true).checkboxradio("refresh");	
+		
+        // Draw circle of area where user is located 		
         var locationArea = drawCircle(userLocation); 
         map.entities.push(locationArea);
     } 
+	
     // Unsuccessful geolocation 
     function locateFail(geoPositionError) { 
+		$("#locationRadios1").attr("disabled", true);
         switch (geoPositionError.code) { 
             case 0: // UNKNOWN_ERROR 
                 alert('An unknown error occurred, sorry'); 
@@ -93,17 +115,44 @@ function GetMap() {
 			if (myLocPin !== null){
 				map.entities.pop();
 			}
-			var userLocation = new Microsoft.Maps.Location(locTemp.latitude, locTemp.longitude);
-            //alert(locTemp.latitude+"&"+locTemp.longitude);
+			
+			//set location to the location chosen by the user
+			chosenLocation = new Microsoft.Maps.Location(locTemp.latitude, locTemp.longitude);
+			userLocation = chosenLocation;
+			
+			//enable "Selbstgewählter Standort" radio button and check it
+			$("#locationRadios2").attr("disabled", false);	
+			$("#locLabel2").removeClass("read-only-list");
+			$("#locationRadios2").attr("checked",true).checkboxradio("refresh");
+			$("#locationRadios1").attr("checked",false).checkboxradio("refresh");            
 
-            var myLocPin = new Microsoft.Maps.Pushpin(userLocation, {'draggable': false});
-
+			//add new pin for location chosen by user
+            var myLocPin = new Microsoft.Maps.Pushpin(userLocation, {text: '2'}, {'draggable': false});
             map.entities.push(myLocPin);
-          //alert("Zum Ändern der Location bitte Pin verschieben");
 
         }              
-       }
+    }
 	
+	//radio button clicked
+	function switchLocation(locationRadio) {
+		//"Gerätestandort" radio button
+		if (locationRadio.value == 1) {
+			if (deviceLocation !== null && deviceLocation !== undefined) {
+				userLocation = deviceLocation;
+				locationRadio.checked = true;
+			}
+		}
+		//"Selbstgewählter Standort" radio button
+		if (locationRadio.value == 2) {
+			if (chosenLocation !== null){
+				userLocation = chosenLocation;				
+				$("#locationRadios2").attr("checked",true).checkboxradio("refresh");
+				$("#locationRadios1").attr("checked",false).checkboxradio("refresh");
+			}
+		}
+	}
+	
+	//getLocation to pass current location to backend
 	function getLocation() {
 		return userLocation;
 	}

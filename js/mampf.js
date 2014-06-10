3d3e1a0ea0b174a786a287916fc686885bac5201
KeSa92 <Kevin.Sandermann@googlemail.com>
2014-06-10 10:47:21 +0200

    added comments on UI-methods

--- js/mampf.js
+++ js/mampf.js
@@ -1,11 +1,14 @@
+/*Append and show the panel on Page-Initiation*/
 $(function() {
 	$( "body>[data-role='panel']" ).panel();
 });
 
+/*Enhance all the children of all elements in the set of matched elements.*/
 $(function () {
   $("[data-role=panel]").panel().enhanceWithin();
 });
 
+/*on click on page in panel -> removeClass "date persist" -> Mark active Page on Panel as red */
 $(function() {
 	$("#left-panel").on("click", "a",function(e) {
 		$(".ui-panel-nav").removeClass('ui-panel-active ui-date-persist');

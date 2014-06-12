701fa83f6349c456ac10ebb39cc1ff0f4e7b05b8
Backbone-js-Demo <Backbone-js-Demo@users.noreply.github.com>
2014-06-12 12:03:49 +0200

    Bugfix: keine Zeit angegeben

--- js/time-overview-page.js
+++ js/time-overview-page.js
@@ -76,58 +76,65 @@ function SaveMyTime(){
 				event.preventDefault();
 			}
 			else{
-				
-				//Check if end time is before start time
-				if(myDate.FromTime > myDate.ToTime){
-					alert("Bitte die Zeiten anpassen");
+				//check if times are valid
+				var Regex = /Invalid Date/;
+				if (Regex.exec(myDate.FromTime) || (Regex.exec(myDate.ToTime))){
+					alert("Bitte einen Zeitrahmen angeben");
 					event.preventDefault();
 				}
 				else{
-					//check if duration is >=45 minutes
-					if((((myDate.ToTime-myDate.FromTime)/1000)/60)<45){
-						alert("Bitte nehmen Sie sich mindestens 45 min Zeit zum Essen!");
+					//Check if end time is before start time
+					if(myDate.FromTime > myDate.ToTime){
+						alert("Bitte die Zeiten anpassen");
 						event.preventDefault();
 					}
 					else{
-					
-						//Create a new JSON Object containing the date and time values 
-						var JSONdate = JSON.stringify(myDate);
-						
-						/*
-						if there are already saved values, read them and append the new value, otherwise 
-						just set old to the current JSON object
-						*/
-						var old = localStorage.getItem("dates");
-							
-						if(old!=null){
-							old = old.substring(0, old.length-1) + "," + JSONdate + "]";
-							var dates=JSON.parse(localStorage.dates);
-							
-							//set idcounter to dates.length
-							idcounter=dates.length;
+						//check if duration is >=45 minutes
+						if((((myDate.ToTime-myDate.FromTime)/1000)/60)<45){
+							alert("Bitte nehmen Sie sich mindestens 45 min Zeit zum Essen!");
+							event.preventDefault();
 						}
 						else{
-							old= "[" + JSONdate + "]";
+						
+							//Create a new JSON Object containing the date and time values 
+							var JSONdate = JSON.stringify(myDate);
+							
+							/*
+							if there are already saved values, read them and append the new value, otherwise 
+							just set old to the current JSON object
+							*/
+							var old = localStorage.getItem("dates");
+								
+							if(old!=null){
+								old = old.substring(0, old.length-1) + "," + JSONdate + "]";
+								var dates=JSON.parse(localStorage.dates);
+								
+								//set idcounter to dates.length
+								idcounter=dates.length;
+							}
+							else{
+								old= "[" + JSONdate + "]";
+							}
+							
+							//save the values to localStorage
+							localStorage.dates=old;
+							
+							//fill up minutes with zeros
+							var fromMinutes = myDate.FromTime.getMinutes();
+							fromMinutes = fromMinutes > 9 ? fromMinutes : '0' + fromMinutes;
+							var toMinutes = myDate.ToTime.getMinutes();
+							toMinutes = toMinutes > 9 ? toMinutes : '0' + toMinutes;
+							
+							//create output
+							$( "#time-over-list" ).append('<li id="'+ idcounter +'" class="editTime"><a class="read-only-list-time" href="#time-edit-page" > '+myDate.FromTime.getDate()+'.'+(myDate.FromTime.getMonth()+1)+'.'+myDate.FromTime.getUTCFullYear()+' von '+(myDate.FromTime.getHours())+':'+fromMinutes+' bis '+(myDate.ToTime.getHours())+':'+toMinutes+'</a><a href="#popup-times" class="deleteTime" data-rel="popup"  data-rel="popup" data-position-to="window" data-transition="fade" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-icon="delete" data-iconpos="right" id="'+ idcounter +'">Delete</a></li>');
+							idcounter++;
+							
+							// Remove information message, if visible
+							$('#time-content-msg').remove(); 
+							
+							//reload page to update changes
+							$( "#time-over-page" ).trigger("pagecreate");			
 						}
-						
-						//save the values to localStorage
-						localStorage.dates=old;
-						
-						//fill up minutes with zeros
-						var fromMinutes = myDate.FromTime.getMinutes();
-						fromMinutes = fromMinutes > 9 ? fromMinutes : '0' + fromMinutes;
-						var toMinutes = myDate.ToTime.getMinutes();
-						toMinutes = toMinutes > 9 ? toMinutes : '0' + toMinutes;
-						
-						//create output
-						$( "#time-over-list" ).append('<li id="'+ idcounter +'" class="editTime"><a class="read-only-list-time" href="#time-edit-page" > '+myDate.FromTime.getDate()+'.'+(myDate.FromTime.getMonth()+1)+'.'+myDate.FromTime.getUTCFullYear()+' von '+(myDate.FromTime.getHours())+':'+fromMinutes+' bis '+(myDate.ToTime.getHours())+':'+toMinutes+'</a><a href="#popup-times" class="deleteTime" data-rel="popup"  data-rel="popup" data-position-to="window" data-transition="fade" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-icon="delete" data-iconpos="right" id="'+ idcounter +'">Delete</a></li>');
-						idcounter++;
-						
-						// Remove information message, if visible
-						$('#time-content-msg').remove(); 
-						
-						//reload page to update changes
-						$( "#time-over-page" ).trigger("pagecreate");			
 					}
 				}
 			}
